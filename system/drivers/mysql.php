1c76663c3cd6047a27758660addfad68a9e820c4
Benjamin Falk <falk@citrosaft.com>
2012-04-02 00:14:16 +0200

    [add] MySQL Driver

---
+++ system/drivers/mysql.php
@@ -0,0 +1,31 @@
+<?php
+	class MysqlDriver implements CI_Database {
+		public function query($sql) {
+			
+		}
+		
+		public function insert($table, $data) {
+			
+		}
+		
+		public function update($table, $data, $options) {
+			
+		}
+		
+		public function delete($table, $data) {
+			
+		}
+		
+		public function truncate($table) {
+			
+		}
+		
+		public function insertId() {
+			
+		}
+		
+		public function numRows($query) {
+			
+		}
+	}
+?>
